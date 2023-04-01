d8d47acd2691bd45c588e51c2c71aa7269b421b3
Oliver Berning <oliverberning@MacBook-Pro-von-Oliver.fritz.box>
2023-04-01 21:40:07 +0200

    Fix route to singular

--- src/service/routes.rs
+++ src/service/routes.rs
@@ -2,7 +2,7 @@ use crate::service::question;
 use actix_web::{get, HttpResponse, Responder, HttpRequest, web::Data, web::Path};
 use sea_orm::DatabaseConnection;
 
-#[get("/questions/{id}")]
+#[get("/question/{id}")]
 async fn root(path: Path<i32>, _req: HttpRequest, db_pool: Data<DatabaseConnection>) -> impl Responder {
     match question::question(path.into_inner(), &db_pool).await {
         Ok(question) => HttpResponse::Ok().body(question),
